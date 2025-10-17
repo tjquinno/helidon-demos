8943a534c723374b2ddfbef5e3c67861385825d8
Tim Quinn <tim.quinn@oracle.com>
2025-10-17 15:33:18 -0500

    Initial commit of SE and MP metrics demos

---
+++ mp/helidon-labs-apps-metrics-quickstart-mp/src/main/java/io/helidon/labs/apps/metrics/quickstart/mp/package-info.java
@@ -0,0 +1,2 @@
+
+package io.helidon.labs.apps.metrics.quickstart.mp;
---
+++ se/helidon-labs-apps-metrics-quickstart-se/src/test/java/io/helidon/labs/apps/metrics/quickstart/se/MainTest.java
@@ -0,0 +1,11 @@
+package io.helidon.labs.apps.metrics.quickstart.se;
+
+import io.helidon.webserver.testing.junit5.DirectClient;
+import io.helidon.webserver.testing.junit5.RoutingTest;
+
+@RoutingTest
+class MainTest extends AbstractMainTest {
+    MainTest(DirectClient client) {
+        super(client);
+    }
+}
